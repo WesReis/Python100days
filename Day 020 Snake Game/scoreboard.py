7031e3fa44c9b86c963f4f9e2c7eebdbc7e04189
Wes Reis <78430439+WesReis@users.noreply.github.com>
2021-02-22 18:30:31 +0000

    Add files via upload

---
+++ Day 020 Snake Game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Comic Sans MS", 20, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.speed("fastest")
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def refresh(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
